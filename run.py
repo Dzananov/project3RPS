f7c206473c77495382a8b5da65877e4bc409589a
Dzananov <danni_daa@hotmail.com>
2022-08-25 19:24:51 +0000

    changdes alot in py code.game working but not percekt

--- run.py
+++ run.py
@@ -3,46 +3,47 @@ import random
 options = ["rock", "scissor", "paper"]
 
 
-class RockScissorPaper:
-    def __init__(self):
-        self.user_win = 0
-        self.computer_win = 0
+def choose_option():
+    """finding out user choise"""
+    user_choise = input("Choose your answer: ")
+    if user_choise in ["rock"]:
+        user_choise = "rock"
+    elif user_choise in ["scissor"]:
+        user_choise = "scissor"
+    elif user_choise in ["paper"]:
+        user_choise = "paper"
+    else:
+        print("wrong input")
+        choose_option()
+    return user_choise
+
+
+def computer_choise():
+    """finding out computer choise"""
+    comp_choise = random.choice(options)
+    return comp_choise
 
 
 while True:
-    def User_input():
-        User_input = input("Choose your option(rock,scissor,paper)").lower()
-        if User_input not in options:
-            print("Try again, choose from the options!")
-        User_input()
+    user_choise = choose_option()
+    comp_choise = computer_choise()
 
-    def computer_input():
-        computer_input = random.choice(options)
-        print(f"You chose {User_input()}, computer chose {computer_input}.")
-
-    def check_winner():
-        if User_input() == computer_input():
-            print(f"Both selected {User_input()}. It's a tie!")
-        elif User_input() == "paper":
-            if computer_input() == "rock":
-                self.user_win += 1
-                print("paper covers rock! You win!")
-            else:
-            self.computer_win += 1
+    if user_choise == comp_choise:
+        print(f"Both selected {user_choise}. It's a tie!")
+    elif user_choise == "paper":
+        if comp_choise == "rock":
+            print("paper covers rock! You win!")
+        else:
             print("scissors cut paper! You lose.")
 
-        elif User_input() == "scissors":
-            if computer_input() == "paper":
-                self.user_win += 1
-                print("scissor cut paper! You win!")
-            else:
-                self.computer_win += 1
-                print("Rock smashes scissors! You lose.")
+    elif user_choise == "scissors":
+        if comp_choise == "paper":
+            print("scissor cut paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
 
-        elif User_input() == "rock":
-            if computer_input() == "scissors":
-                self.user_win += 1
-                print("Rock smashes scissors! You win")
-            else:
-                self.computer_win += 1
-                print("paper covers rock! You lose.")
+    elif user_choise == "rock":
+        if comp_choise == "scissors":
+            print("Rock smashes scissors! You win")
+        else:
+            print("paper covers rock! You lose.")
